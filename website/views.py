687ec73934f0c4be5a4e19f05e656b37a9771e87
Khalil Ahmad Qamar <khalilq199@gmail.com>
2025-06-01 20:34:15 -0400

    Website is mostly done, will continue improving it

--- website/views.py
+++ website/views.py
@@ -1,15 +1,44 @@
 # this file stores standard routes for our website, anywhere users can actually go (Home page etc..)
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 # define that this file is a blueprint for the application, it has routes inside of it
 
 views = Blueprint("views", __name__) # setup a blueprint for the flask application
 
-@views.route("/") # "/" route
+@views.route("/", methods=['GET', 'POST']) # "/" route
 # this home function will be called when the "/" route is entered
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST': 
+        note = request.form.get('note')#Gets the note from the HTML 
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            db.session.add(new_note) #adding the note to the database 
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
 
 
 
